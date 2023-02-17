089d4092c6b136d6f58303f9fe3de08bb3c7256f
saadrashid304 <saadrashid304@gmail.com>
2023-02-17 19:56:10 +0500

    Published

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.mainContainer}>
+      <div className={styles.cardFront}>
+        <div className={styles.filledCircle} />
+        <div className={styles.emptyCircle} />
+        <p className={styles.cardNumber}>{`0000 0000 0000 0000`}</p>
+        <p className={styles.cardName}>{`SAAD RASHID`}</p>
+        <p className={styles.cardDate}>{`00/00`}</p>
+      </div>
+      <div className={styles.cardBack}>
+        <p className={styles.cvvNo}>{`000`}</p>
+      </div>
+      <div className={styles.formContainer}>
+        <div className={styles.inputFieldContainer}>
+          <label>CARDHOLDER NAME</label>
+          <input type="text" placeholder="e.g. SAAD RASHID" />
+        </div>
+        <div className={styles.inputFieldContainer}>
+          <label>CARD NUMBER</label>
+          <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
